823dc497979b0b41bc95180b86124a79d28e7f40
Behram <ikhanbehram@gmail.com>
2022-12-27 19:14:06 +0500

    navbar completed

---
+++ src/components/AuthModal.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref, defineProps } from 'vue';
+
+const { isLogin } = defineProps(['isLogin']);
+
+const visible = ref(false);
+
+const showModal = () => {
+	visible.value = true;
+};
+
+const handleOk = (e) => {
+	visible.value = false;
+};
+
+const title = isLogin ? 'Login' : 'Signup';
+</script>
+
+<template>
+	<div>
+		<a-button type="primary" class="btn" @click="showModal">{{ title }}</a-button>
+		<a-modal v-model:visible="visible" :title="title" @ok="handleOk">
+			<a-input class="input" v-if="!isLogin" v-model:value="value" placeholder="User Name" />
+			<a-input class="input" v-model:value="value" placeholder="Email" />
+			<a-input class="input" v-model:value="value" placeholder="Password" />
+		</a-modal>
+	</div>
+</template>
+
+<style scoped>
+.btn {
+	margin: 0px 10px;
+}
+.input {
+	margin: 8px 0px;
+}
+</style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,8 +2,10 @@
 import { RouterLink } from 'vue-router';
 import Container from '../layouts/Container.vue';
 import { ref } from 'vue';
+import AuthModal from './AuthModal.vue';
 
 const searchUserName = ref('');
+const isAuthenticated = ref(false);
 
 const onSearch = () => {};
 </script>
@@ -21,9 +23,13 @@ const onSearch = () => {};
 						@search="onSearch"
 					/>
 				</div>
-				<div class="left-content">
-					<a-button type="primary">Signup</a-button>
-					<a-button type="primary" ghost>Login</a-button>
+				<div class="left-content" v-if="!isAuthenticated">
+					<AuthModal :isLogin="false"/>
+					<AuthModal :isLogin="true"/>
+				</div>
+				<div class="left-content" v-else>
+					<a-button type="primary">Profile</a-button>				
+					<a-button type="primary" ghost>Logout</a-button>				
 				</div>
 			</div>
 		</Container>
@@ -45,7 +51,11 @@ const onSearch = () => {};
 	margin: 0px 10px;
 }
 
-.left-content button{
-	margin: 0px 10px;
+.left-content {
+	display: flex;
+	align-items: center;
+}
+.left-content button {
+	margin: 10px 0px;
 }
 </style>
