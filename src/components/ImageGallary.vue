ade96a3d5f34a5deb19c45f9e50d3979a3b8f22e
Behram <ikhanbehram@gmail.com>
2023-01-08 18:02:33 +0500

    UI is almost finished

---
+++ src/components/ImageGallary.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const { posts } = defineProps(['posts']);
+</script>
+
+<template>
+	<div class="image-gallary-container">
+		<img v-for="post in posts" :key="post.id" :src="post.image" />
+	</div>
+</template>
+
+<style scoped>
+.image-gallary-container {
+	display: flex;
+	justify-content: left;
+	flex-wrap: wrap;
+}
+img {
+	margin: 5px;
+	width: 200px;
+}
+</style>
--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,5 +1,6 @@
 <script setup>
 import Container from '../layouts/Container.vue';
+import ImageGallary from './ImageGallary.vue';
 import UserBar from './UserBar.vue';
 </script>
 
@@ -14,6 +15,16 @@ import UserBar from './UserBar.vue';
 					following: 200
 				}"
 			/>
+			<ImageGallary :posts="[
+				{
+					id: 1,
+					image: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80'
+				},
+				{
+					id: 2,
+					image: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80'
+				}
+			]"/>
 		</div>
 	</Container>
 </template>
