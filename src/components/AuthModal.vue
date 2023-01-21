8fb43820fd6a5d91210a33aef2451b74f4a1d23a
Behram <77900065+ikhanbehram@users.noreply.github.com>
2023-01-22 02:15:52 +0500

    done with signup flow

--- src/components/AuthModal.vue
+++ src/components/AuthModal.vue
@@ -1,86 +1,97 @@
 <script setup>
-import { ref, reactive, defineProps } from 'vue';
-import { useUserStore } from '../stores/users';
-import { storeToRefs } from 'pinia';
+import { ref, reactive, defineProps } from "vue";
+import { useUserStore } from "../stores/users";
+import { storeToRefs } from "pinia";
 
 const userStore = useUserStore();
 
-const { errorMessage, loading } = storeToRefs(userStore);
+const { errorMessage, user, loading } = storeToRefs(userStore);
 
-const { isLogin } = defineProps(['isLogin']);
+const { isLogin } = defineProps(["isLogin"]);
 const useCredentials = reactive({
-	email: '',
-	password: '',
-	username: ''
+  email: "",
+  password: "",
+  username: "",
 });
 const visible = ref(false);
 
-const showModal = () => {
-	visible.value = true;
+const clearUserInputs = () => {
+  useCredentials.email = "";
+  useCredentials.password = "";
+  useCredentials.username = "";
+  userStore.clearErrorMessage();
 };
 
-const handleOk = () => {
-	userStore.handleSignup(useCredentials);
-	// visible.value = false;
+const showModal = () => {
+  visible.value = true;
+};
+
+const handleOk = async () => {
+  await userStore.handleSignup(useCredentials);
+if (user.value) {
+    visible.value = false;
+    clearUserInputs();
+  }
 };
 const handleCancel = () => {
-	visible.value = false;
+  visible.value = false;
+  clearUserInputs();
 };
 
-const title = isLogin ? 'Login' : 'Signup';
+const title = isLogin ? "Login" : "Signup";
 </script>
 
 <template>
-	<div>
-		<a-button type="primary" class="btn" @click="showModal">{{
-			title
-		}}</a-button>
-		<a-modal v-model:visible="visible" :title="title">
-			<template #footer>
-				<a-button key="back" @click="handleCancel">Cancel</a-button>
-				<a-button
-					key="submit"
-					type="primary"
-					:loading="loading"
-					@click="handleOk"
-					:disabled="loading"
-					>Submit</a-button
-				>
-			</template>
-			<div>
-				<a-input
-					class="input"
-					v-if="!isLogin"
-					v-model:value="useCredentials.username"
-					placeholder="User name"
-					:disabled="loading"
-				/>
-				<a-input
-					class="input"
-					v-model:value="useCredentials.email"
-					placeholder="Email"
-					:disabled="loading"
-				/>
-				<a-input
-					class="input"
-					v-model:value="useCredentials.password"
-					type="password"
-					placeholder="Password"
-					:disabled="loading"
-				/>
-			</div>
-			<a-typography-text type="danger" v-if="errorMessage">{{
-				errorMessage
-			}}</a-typography-text>
-		</a-modal>
-	</div>
+  <div>
+    <a-button type="primary" class="btn" @click="showModal">{{
+      title
+    }}</a-button>
+    <a-modal v-model:visible="visible" :title="title">
+      <template #footer>
+        <a-button key="back" @click="handleCancel">Cancel</a-button>
+        <a-button
+          key="submit"
+          type="primary"
+          :loading="loading"
+          @click="handleOk"
+          :disabled="loading"
+          >Submit</a-button
+        >
+      </template>
+      <div>
+        <a-input
+          class="input"
+          v-if="!isLogin"
+          v-model:value="useCredentials.username"
+          placeholder="User name"
+          :disabled="loading"
+        />
+        <a-input
+          class="input"
+          v-model:value="useCredentials.email"
+          placeholder="Email"
+          :disabled="loading"
+        />
+        <a-input
+          class="input"
+          v-model:value="useCredentials.password"
+          type="password"
+          placeholder="Password"
+          :disabled="loading"
+        />
+      </div>
+      <a-typography-text type="danger" v-if="errorMessage">{{
+        errorMessage
+      }}</a-typography-text>
+    </a-modal>
+  </div>
 </template>
 
 <style scoped>
 .btn {
-	margin: 0px 10px;
+  margin: 0px 10px;
 }
 .input {
-	margin: 8px 0px;
+  margin: 8px 0px;
 }
 </style>
--- src/stores/users.js
+++ src/stores/users.js
@@ -1,74 +1,91 @@
-import { ref } from 'vue';
-import { defineStore } from 'pinia';
-import { supabase } from '../supabase';
+import { ref } from "vue";
+import { defineStore } from "pinia";
+import { supabase } from "../supabase";
 
 const validateEmail = (email) => {
-	return String(email)
-		.toLowerCase()
-		.match(
-			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-		);
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
 };
 
-export const useUserStore = defineStore('users', () => {
-	const user = ref(null);
-	const errorMessage = ref('');
-	const loading = ref(false);
+export const useUserStore = defineStore("users", () => {
+  const user = ref(null);
+  const errorMessage = ref("");
+  const loading = ref(false);
 
-	const handleLogin = () => {};
-	const handleSignup = async (creds) => {
-		const { email, password, username } = creds;
+  const handleLogin = () => {};
+  const handleSignup = async (creds) => {
+    const { email, password, username } = creds;
 
-		if (password.length < 6) {
-			return (errorMessage.value =
-				'Password length is cannot be less than 6 characters');
-		}
-		if (username.length < 4) {
-			return (errorMessage.value = 'Username is too short');
-		}
-		if (!validateEmail(email)) {
-			return (errorMessage.value = 'Not a valid email');
-		}
-		loading.value = true;
+    if (password.length < 6) {
+      return (errorMessage.value =
+        "Password length is cannot be less than 6 characters");
+    }
+    if (username.length < 4) {
+      return (errorMessage.value = "Username is too short");
+    }
+    if (!validateEmail(email)) {
+      return (errorMessage.value = "Not a valid email");
+    }
+    loading.value = true;
 
-		const { data: userWithUsername } = await supabase
-			.from('users')
-			.select()
-			.eq('username', username)
-			.single();
+    const { data: userWithUsername } = await supabase
+      .from("users")
+      .select()
+      .eq("username", username)
+      .single();
 
-		if (userWithUsername) {
-			loading.value = false;
-			return errorMessage.value = "User already resgistered";
-		}
-		const { error } = await supabase.auth.signUp({
-			email,
-			password
-		});
-		
-		if (error) {
-			loading.value = false;
-			return (errorMessage.value = error.message);
-		}
-		
-		await supabase.from('users').insert({
-			username,
-			email
-		});
-		
-		loading.value = false;
-		errorMessage.value = '';
-	};
-	const handleLogout = () => {};
-	const getUser = () => {};
+    if (userWithUsername) {
+      loading.value = false;
+      return (errorMessage.value = "User already resgistered");
+    }
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
 
-	return {
-		user,
-		errorMessage,
-		loading,
-		handleLogin,
-		handleSignup,
-		handleLogout,
-		getUser
-	};
+    if (error) {
+      loading.value = false;
+      return (errorMessage.value = error.message);
+    }
+
+    await supabase.from("users").insert({
+      username,
+      email,
+    });
+
+    const { data: newUser } = await supabase
+      .from("users")
+      .select()
+      .eq("username", username)
+      .single();
+console.log(newUser);
+    user.value = {
+      id: newUser.id,
+      email: newUser.email,
+      username: newUser.username,
+    };
+
+    loading.value = false;
+  };
+  const handleLogout = () => {};
+  const getUser = () => {};
+
+  const clearErrorMessage = () => {
+    errorMessage.value = "";
+  };
+
+  return {
+    user,
+    errorMessage,
+    loading,
+    user,
+    handleLogin,
+    handleSignup,
+    handleLogout,
+    getUser,
+	clearErrorMessage
+  };
 });
