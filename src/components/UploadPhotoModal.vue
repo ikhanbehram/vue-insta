4d469671621ccfe672bad9937edfcccf39d97ff0
Behram <77900065+ikhanbehram@users.noreply.github.com>
2023-01-22 23:58:27 +0500

    uploading file to bucket in progress

--- src/components/UploadPhotoModal.vue
+++ src/components/UploadPhotoModal.vue
@@ -1,35 +1,40 @@
 <script setup>
-import { file } from '@babel/types';
-import { ref } from 'vue';
+import { supabase } from "../supabase";
+import { ref } from "vue";
 
 const visible = ref(false);
 const caption = ref("");
+const file = ref(null);
 
 const showModal = () => {
-    visible.value = true;
-}
+  visible.value = true;
+};
 
-const handleOk = () => {
-    visible.value = false;
-}
+const handleOk = async() => {
+    const fileName = Math.floor(Math.random() * 100000000000);
+    if (file.value) {
+        await supabase.storage.from("images").upload('public/' + fileName, file.value);
+    }
+//   visible.value = false;
+};
 
 const handleChange = (e) => {
-    if (e.target.files[0]) {
-        file.value = e.target.files[0]
-    }
-}
+  if (e.target.files[0]) {
+    file.value = e.target.files[0];
+  }
+};
 </script>
 <template>
   <div>
-    <a-button  @click="showModal">Upload Photo</a-button>
+    <a-button @click="showModal">Upload Photo</a-button>
     <a-modal v-model:visible="visible" title="Upload Photo" @ok="handleOk">
-        <input type="file" accept=".jpeg,.png,.jgp" @change="handleChange"/>
-        <a-input placeholder="Caption" :maxLength="50" v-model:value="caption"/>
+      <input type="file" accept=".jpeg,.png,.jgp" @change="handleChange" />
+      <a-input placeholder="Caption" :maxLength="50" v-model:value="caption" />
     </a-modal>
   </div>
 </template>
 <style scoped>
 input {
-    margin-top: 10px;
+  margin-top: 10px;
 }
 </style>
