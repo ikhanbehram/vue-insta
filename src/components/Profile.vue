5888f646a0edadbe2ea6ca6ea2bb1e07a0de786b
Behram <77900065+ikhanbehram@users.noreply.github.com>
2023-01-28 23:32:28 +0500

    bug fixed follow button added

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -8,6 +8,7 @@ import { useRoute } from "vue-router";
 
 const posts = ref([]);
 const user = ref(null);
+const loading = ref(false);
 
 const route = useRoute();
 
@@ -18,30 +19,42 @@ const addNewPost = (post) => {
 };
 
 const fetchData = async () => {
-  const {data: userData} = await supabase
+  loading.value = true;
+  const { data: userData } = await supabase
     .from("users")
     .select()
     .eq("username", username)
     .single();
+
+  if (!userData) {
+    loading.value = false;
+    return (user.value = null);
+  }
   user.value = userData;
 
-  const {data: postsData} = await supabase.from("posts").select("fk_user_id", user.value.id);
+  const { data: postsData } = await supabase
+    .from("posts")
+    .select()
+    .eq('fk_user_id', user.value.id);
+
+  posts.value = postsData;
+  loading.value = false;
 };
 
-onMounted(()=>{
+onMounted(() => {
   fetchData();
 });
 </script>
 
 <template>
   <Container>
-    <div class="profile-container">
-      <UserBar
-        :key="username"
-        :user="user"
-        :addNewPost="addNewPost"
-      />
+    <div class="profile-container" v-if="!loading">
+      <UserBar :key="username" :user="user" :addNewPost="addNewPost" />
       <ImageGallary :posts="posts" />
+    </div>
+
+    <div class="spinner" v-else>
+      <a-spin />
     </div>
   </Container>
 </template>
@@ -51,5 +64,11 @@ onMounted(()=>{
   display: flex;
   flex-direction: column;
   padding: 10px 0px;
+}
+.spinner {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 85vh;
 }
 </style>
--- src/components/UserBar.vue
+++ src/components/UserBar.vue
@@ -1,24 +1,28 @@
 <script setup>
-import { defineProps } from 'vue';
-import UploadPhotoModal from './UploadPhotoModal.vue';
-import { useRoute } from 'vue-router';
-import { useUserStore } from '../stores/users';
-import { storeToRefs } from 'pinia';
+import { defineProps } from "vue";
+import UploadPhotoModal from "./UploadPhotoModal.vue";
+import { useUserStore } from "../stores/users";
+import { storeToRefs } from "pinia";
 
-const route = useRoute();
 const userStore = useUserStore();
 
-const {user} = storeToRefs(userStore);
+const { user: currentUser } = storeToRefs(userStore);
 
-const { user: propUser, addNewPost } = defineProps(['user', 'addNewPost']);
+const { user, addNewPost } = defineProps(["user", "addNewPost"]);
 </script>
 <template>
-	<div class="userbar-container" v-if="propUser">
-		<div class="top-content">
-			<a-typography-title :level="2">{{ propUser.username }}</a-typography-title>
-			<UploadPhotoModal v-if="user && propUser.username === user.username" :addNewPost="addNewPost"/>
-		</div>
-		<div class="bottom-content">
+  <div class="userbar-container" v-if="user">
+    <div class="top-content">
+      <a-typography-title :level="2">{{ user.username }}</a-typography-title>
+      <div v-if="currentUser">
+        <UploadPhotoModal
+          v-if="currentUser.username === user.username"
+          :addNewPost="addNewPost"
+        />
+        <a-button v-else>Follow</a-button>
+      </div>
+    </div>
+    <!-- <div class="bottom-content">
 			<a-typography-title :level="5"
 				>{{ user.posts }} Posts</a-typography-title
 			>
@@ -28,33 +32,32 @@ const { user: propUser, addNewPost } = defineProps(['user', 'addNewPost']);
 			<a-typography-title :level="5"
 				>{{ user.following }} Following</a-typography-title
 			>
-		</div>
-	</div>
+		</div> -->
+  </div>
 
-	<div class="userbar-container" v-else>
-		<div class="top-content">
-			<a-typography-title :level="2">user not found</a-typography-title>
-			
-		</div>
-	</div>
+  <div class="userbar-container" v-else>
+    <div class="top-content">
+      <a-typography-title :level="2">user not found</a-typography-title>
+    </div>
+  </div>
 </template>
 
 <style scoped>
 .userbar-container {
-	padding-bottom: 75px;
+  padding-bottom: 75px;
 }
 .top-content {
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 .bottom-content {
-	display: flex;
-	align-items: center;
+  display: flex;
+  align-items: center;
 }
 .bottom-content h5 {
-	margin: 0 !important;
-	padding: 0;
-	margin-right: 30px !important;
+  margin: 0 !important;
+  padding: 0;
+  margin-right: 30px !important;
 }
 </style>
